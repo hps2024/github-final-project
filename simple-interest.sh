bf5890626446f6d6ab7ddfdc5e8db8d2de60ca3d
Harshad Sawant <156855153+hps2024@users.noreply.github.com>
2026-07-11 16:07:35 -0400

    Update simple-interest.sh: add hps2024 as additional author

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,28 +1,28 @@
-   #!/bin/bash
-   # This script calculates simple interest given principal,
-   # annual rate of interest and time period in years.
+#!/bin/bash
+# This script calculates simple interest given principal,
+# annual rate of interest and time period in years.
 
-   # Do not use this in production. Sample purpose only.
+# Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+# Author: Upkar Lidder (IBM)
+# Additional Authors:
+# hps2024
 
-   # Input:
-   # p, principal amount
-   # t, time period in years
-   # r, annual rate of interest
+# Input:
+# p, principal amount
+# t, time period in years
+# r, annual rate of interest
 
-   # Output:
-   # simple interest = p*t*r
+# Output:
+# simple interest = p*t*r
 
-   echo "Enter the principal:"
-   read p
-   echo "Enter time period in years:"
-   read t
-   echo "Enter rate of interest per year:"
-   read r
+echo "Enter the principal:"
+read p
+echo "Enter time period in years:"
+read t
+echo "Enter rate of interest per year:"
+read r
 
-   s=$(echo "scale=2; $p * $t * $r / 100" | bc)
-   echo "The simple interest is: "
-   echo $s
+s=$(echo "scale=2; $p * $t * $r / 100" | bc)
+echo "The simple interest is: "
+echo $s
